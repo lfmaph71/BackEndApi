be5910a3fb62a9dbf95df0a0ca2dee908ddaf0ae
Leonardo Martinez <lfmaph71@users.noreply.github.com>
2023-11-24 16:27:02 -0500

    corregir

--- BackEndApi/Models/Usuario.cs
+++ BackEndApi/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BackEndApi.Models;
 
@@ -7,9 +8,14 @@ public partial class Usuario
 {
     public int IdUsuario { get; set; }
 
+    [Required(ErrorMessage ="Nombre es Requerido")]
+    [MaxLength(100,ErrorMessage ="El nombre Tiene mas de 100 caracteres")]
     public string Nombres { get; set; } = null!;
 
+    [Required(ErrorMessage ="Correo es Requerido")]
+    [MaxLength(50,ErrorMessage ="El nombre Tiene mas de 100 caracteres")]
     public string Correo { get; set; } = null!;
 
+    [Required(ErrorMessage ="Edad es Requerido")]
     public int Edad { get; set; }
 }
